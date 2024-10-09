24da96a41dbf7a04517bc3965a3d0972feb05996
Diana Tang <1987567948@qq.com>
2024-10-09 19:29:17 +0800

    feat: change port

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,24 @@
 /*
  * @Author: Diana Tang
- * @Date: 2024-10-09 16:41:35
+ * @Date: 2024-10-09 17:11:54
  * @LastEditors: Diana Tang
  * @Description: some description
  * @FilePath: /Diana-s-tech-blog/backend/server.js
  */
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // 引入 CORS
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // 连接 MongoDB 数据库
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/blog')
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+// 中间件
+app.use(cors()); // 允许所有来源的跨域请求
+app.use(express.json());
 
 // 博客文章模型
 const PostSchema = new mongoose.Schema({
@@ -22,9 +27,6 @@ const PostSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now }
 });
 const Post = mongoose.model('Post', PostSchema);
-
-// 中间件
-app.use(express.json());
 
 // 获取所有博客文章
 app.get('/api/posts', async (req, res) => {
