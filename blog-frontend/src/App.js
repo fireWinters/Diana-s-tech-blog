14d979ad65a5e3cb256c6117744c999eb021485f
Diana Tang <1987567948@qq.com>
2024-10-09 21:39:07 +0800

    feat: 跨域修改端口，6000可能不安全，改成5001，好了。proxy配置不生效，需要进一步排查

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -2,13 +2,17 @@
  * @Author: Diana Tang
  * @Date: 2024-10-09 17:12:04
  * @LastEditors: Diana Tang
- * @Description: some description
+ * @Description: 
+ * ,
+  "proxy": "http://localhost:5001"这个配置在package中不会生效
  * @FilePath: /blog-frontend/src/App.js
  */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import {POSTS} from './utils/Url'
 
+const PROXY="http://localhost:5001"
 function App() {
   const [posts, setPosts] = useState([]);
   const [title, setTitle] = useState('');
@@ -16,17 +20,19 @@ function App() {
 
   useEffect(() => {
     // 获取所有博客文章
-    axios.get('/api/posts').then(res => setPosts(res.data));
+    axios.get(POSTS).then(res => setPosts(res.data))
+    .catch(err => console.error('Error creating post:',err));
   }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     // 创建新的博客文章
-    axios.post('/api/posts', { title, content }).then(res => {
+    axios.post(POSTS, { title, content }).then(res => {
       setPosts([...posts, res.data]);
       setTitle('');
       setContent('');
-    });
+    })
+    .catch(err => console.error('Error creating post:',err));
   };
   return (
     <div className="App">
---
+++ blog-frontend/src/utils/Url.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-10-09 21:33:52
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /blog-frontend/src/utils/Url.js
+ */
+const PROXY="http://localhost:5001"
+export const POSTS=`http://localhost:5001/api/posts`
